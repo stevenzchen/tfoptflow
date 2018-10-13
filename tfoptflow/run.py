187c8366a39078eb2f415e226cb39a8fe8762dac
Steven Chen <stevenzc3@gmail.com>
2018-10-13 09:28:54 -0700

    tfoptflow runscript for generating flow video

--- tfoptflow/run.py
+++ tfoptflow/run.py
@@ -1,29 +1,38 @@
 from __future__ import absolute_import, division, print_function
 import sys
 from model_pwcnet import ModelPWCNet, _DEFAULT_PWCNET_TEST_OPTIONS
-from visualize import display_img_pairs_w_flows
+from visualize import plot_img_pairs_w_flows
 from optflow import flow_to_img, flow_write_as_png
 import argparse
+import cv2
 import imageio
 import numpy as np
-import tensorflow as tf
 from copy import deepcopy
+
+# import stream utility from JITNet utils
+sys.path.append('../../utils')
+from stream import VideoInputStream
+
 
 CKPT_PATH = '../../../pwcnet/pwcnet-lg-6-2-multisteps-chairsthingsmix/pwcnet.ckpt-595000'
 
 def main():
     # parse input and output file paths
     parser = argparse.ArgumentParser(description='Run PWCNet on video.')
-    parser.add_argument('input', help='input file')
-    parser.add_argument('output', help='output directory')
+    parser.add_argument('input', help='input video file')
+    parser.add_argument('output', help='output video file')
     args = parser.parse_args()
+
+    video = VideoInputStream(args.input, start_frame=0)
+    out_video = cv2.VideoWriter(args.output,
+                                cv2.VideoWriter_fourcc(*'DIVX'),
+                                video.rate, (2 * video.width, video.height))
 
     # set NN test options
     nn_opts = deepcopy(_DEFAULT_PWCNET_TEST_OPTIONS)
     nn_opts['verbose'] = True
     nn_opts['ckpt_path'] = CKPT_PATH
     nn_opts['batch_size'] = 1
-    nn_opts['use_tf_data'] = False
     nn_opts['gpu_devices'] = ['/device:GPU:0']
     nn_opts['controller'] = '/device:GPU:0'
     nn_opts['use_dense_cx'] = True
@@ -31,34 +40,28 @@ def main():
     nn_opts['pyr_lvls'] = 6
     nn_opts['flow_pred_lvl'] = 2
 
-    nn = ModelPWCNet(mode='test', options=nn_opts)
-    x_tnsr = nn.x_tnsr
-    y_hat_test_tnsr = nn.y_hat_test_tnsr
-    
-    # read from the input video file
-    video = imageio.get_reader(args.input, 'ffmpeg')
+    # since the model generates flow padded to multiples of 64, reduce back to 1080p
+    # TODO: this should be whatever the input video width and height is
+    nn_opts['adapt_info'] = (1, video.height, video.width, 2)
 
-    # init = tf.global_variables_initializer()
-    # nn.sess.run(init)
+    nn = ModelPWCNet(mode='test', options=nn_opts)
+
     prev = None
     for i, im in enumerate(video):
-        # TODO: is this division necessary?
-        # im /= 255.0
+        print(i)
         if i == 0:
             prev = im
             continue
         # run flow on prev and im
-        img_pairs = np.expand_dims(np.stack((prev, im)), 0)
-        feed_dict = {x_tnsr: img_pairs}
-        y_hat = nn.sess.run(y_hat_test_tnsr, feed_dict=feed_dict)
-        pred_flows, _ = self.postproc_y_hat_test(y_hat)
+        img_pair = [(prev, im)]
+        pred_labels = nn.predict_from_img_pairs(img_pair, batch_size=1, verbose=False)
+        pred = pred_labels[0]
 
-        print('here')
         # show results
-        plt = plot_img_pairs_w_flows(img_pairs, flow_preds=pred_flows)
-        print('ok')
-        plt.savefig('temp.png')
-        return
+        flow_im = flow_to_img(pred)
+
+        vis_image = np.concatenate((im, flow_im), axis=1)
+        out_video.write(vis_image)
 
         prev = im
 
